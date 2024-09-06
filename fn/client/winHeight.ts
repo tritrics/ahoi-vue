c55250883c570fa36f9c95db032300417358dac3
tritrics <ma@tritrics.dk>
2024-09-06 08:36:00 +0200

    fix window width and height calculation

--- fn/client/winHeight.ts
+++ fn/client/winHeight.ts
@@ -1,8 +1,8 @@
 /**
- * Get the window (inner) height.
+ * Get the window height, including or excluding horizontal scroll bar.
  * Get from document is safer for mobile, where especially the window-property changes with
  * showing/hiding the adressbar.
  */
-export default function winHeight(fromDocument: boolean = true): number {
-  return fromDocument ? document.documentElement.clientHeight : window.innerHeight
+export default function winHeight(withScrollbar: boolean = true): number {
+  return withScrollbar ? window.innerHeight : document.documentElement.clientHeight
 }
--- fn/client/winWidth.ts
+++ fn/client/winWidth.ts
@@ -1,8 +1,9 @@
 /**
- * Get the window (inner) width.
+ * Get the window width, including or excluding vertical scroll bar.
+ * With scrollbar = value used in @media queries.
  * Get from document is safer for mobile, where especially the window-property changes with
  * showing/hiding the adressbar.
  */
-export default function winWidth(fromDocument: boolean = true): number {
-  return fromDocument ? document.documentElement.clientWidth : window.innerWidth
+export default function winWidth(withScrollbar: boolean = true): number {
+  return withScrollbar ?  window.innerWidth : document.documentElement.clientWidth
 }
